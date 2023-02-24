a9a03701d0db6551bb06fe99873963b5a59e6ae0
FlorianBury <FlorianBury@github.com>
2023-02-24 17:33:07 +0100

    Now saves root files in data subdir + fire rate implemented

--- timeCal/src/timeCal/PlotScan/plotScan.py
+++ timeCal/src/timeCal/PlotScan/plotScan.py
@@ -24,6 +24,21 @@ from .data_helper import Data, Observable
 
 from ..utils.yamlLoader import parseYaml
 from ..utils.environment import getEnv
+from ..utils.context import TFileOpen
+
+
+OBSERVABLES = ['Efficiency',
+               'Fire rate',
+               'Out-of-time fraction',
+               'Mean',
+               'Next BX contamination',
+               'Previous BX contamination']
+RANGES = {'Efficiency'                 : (0.,1.),
+          'Fire rate'                  : (0.,1.),
+          'Out-of-time fraction'       : (0.,1.),
+          'Mean'                       : (-1.,1.),
+          'Next BX contamination'      : (0.,1.),
+          'Previous BX contamination'  : (0.,1.)}
 
 
 try:
@@ -32,65 +47,65 @@ except:
     ROOT_VERSION = None
 
 class FileProcessor:
-    def __init__(self,hists,efficiency,parameters):
+    def __init__(self,hists,efficiency,firerate,parameters):
         self.hists      = hists
         self.efficiency = efficiency
+        self.firerate   = firerate
         self.parameters = parameters
 
     def __call__(self,f):
         # Open file #
         if not os.path.exists(f):
             raise RuntimeError(f'File {f} does not exist')
-        F = ROOT.TFile(f)
-        #ROOT.SetOwnership(F,False)
+        with TFileOpen(f,'r') as F:
+            # Get file parameters #
+            params = self._getParameters(F)
+            # Produce efficiency hist#
+            h_eff = self._getEfficiencyHist(F)
+            h_fire = self._getFireRate(F)
 
-        # Get file parameters #
-        params = self._getParameters(F)
-        # Produce efficiency hist#
-        h_eff = self._getEfficiencyHist(F)
+            # Loop over each histogram #
+            content = []
+            for name,values in self.hists.items():
+                # Initialize entry #
+                entry = {k:v for k,v in params.items()}
+                entry.update(values)
+                delay = values['delay']
 
-        # Loop over each histogram #
-        content = []
-        for name,values in self.hists.items():
-            # Initialize entry #
-            entry = {k:v for k,v in params.items()}
-            entry.update(values)
-            delay = values['delay']
+                # Get efficiency #
+                eff,contNext,contPrev = self._getEfficiencyValues(h_eff,delay)
+                entry['Efficiency'] = eff
+                entry['Next BX contamination'] = contNext
+                entry['Previous BX contamination'] = contPrev
 
-            # Get efficiency #
-            eff,contNext,contPrev = self._getEfficiencyValues(h_eff,delay)
-            entry['Efficiency'] = eff
-            entry['Next BX contamination'] = contNext
-            entry['Previous BX contamination'] = contPrev
+                # Get firerate #
+                entry['Fire rate'] = self._getFireRateValue(h_fire,delay)
 
-            # Get histogram #
-            h = self._getHistogram(F,values['dir'],name)
+                # Get histogram #
+                h = self._getHistogram(F,values['dir'],name)
 
-            # Get content in numpy arrays #
-            e,w,s = self._getHistContent(h)
+                # Get content in numpy arrays #
+                e,w,s = self._getHistContent(h)
 
-            # Compute additional info based on histogram #
-            fo,fo_err = self._fracOutHisto(e,w,s)
-            entry['Out-of-time fraction'] = fo
-            entry['Out-of-time fraction error'] = fo_err
-            m,m_err = self._meanHisto(e,w,s)
-            entry['Mean'] = m
-            entry['Mean error'] = m_err
+                # Compute additional info based on histogram #
+                fo,fo_err = self._fracOutHisto(e,w,s)
+                entry['Out-of-time fraction'] = fo
+                entry['Out-of-time fraction error'] = fo_err
+                m,m_err = self._meanHisto(e,w,s)
+                entry['Mean'] = m
+                entry['Mean error'] = m_err
 
-            # Save entry in content #
-            content.append(entry)
-
-        # Close file #
-        F.Close()
+                # Save entry in content #
+                content.append(entry)
 
         # return #
         return content
 
     @staticmethod
-    def _getHistogram(F,dirPath,name):
+    def _getHistogram(F,dir_path,name):
         # Get through subdirectories by recurrence #
         subdir = F
-        for sub in dirPath.split('/'):
+        for sub in dir_path.split('/'):
             if ROOT_VERSION is not None and int(ROOT_VERSION[1]) >= 22:
                 if not hasattr(subdir,sub):
                     raise RuntimeError(f'Could not find subdirectory {sub} from {subdir} in file {F.GetName()}')
@@ -99,7 +114,7 @@ class FileProcessor:
                 subdir = subdir.Get(sub)
         if not subdir.GetListOfKeys().FindObject(name):
             raise RuntimeError(f'Could not find histogram {name} from subdir {subdir} in file {F.GetName()}')
-        return subdir.Get(name) 
+        return subdir.Get(name)
 
     def _getParameters(self,F):
         params = {}
@@ -108,17 +123,23 @@ class FileProcessor:
                 raise RuntimeError(f'Could not find parameter {pFile} in file {F.GetName()}')
             params[pName] = float(F.Get(pFile).GetTitle())
         return params
-            
+
     def _getEfficiencyHist(self,F):
-        trueName = os.path.join(self.efficiency['dir'],self.efficiency['truth'])
-        recoName = os.path.join(self.efficiency['dir'],self.efficiency['reco'])
         h_true = self._getHistogram(F,self.efficiency['dir'],self.efficiency['truth'])
         h_reco = self._getHistogram(F,self.efficiency['dir'],self.efficiency['reco'])
-        h_true = F.Get(trueName)
-        h_reco = F.Get(recoName)
-        h_eff = h_reco.Clone("Eff")
+        h_eff  = h_reco.Clone("eff")
         h_eff.Divide(h_true)
         return h_eff
+
+    def _getFireRate(self,F):
+        h_true = self._getHistogram(F,self.firerate['dir'],self.firerate['truth'])
+        h_reco = self._getHistogram(F,self.firerate['dir'],self.firerate['reco'])
+        h_fire = h_reco.ProjectionY("fire",firstxbin=2) # BX = 0 -> no hit
+        h_true = h_true.ProjectionY("",
+                                    firstxbin  = h_true.GetXaxis().FindBin(0.),
+                                    lastxbin   = h_true.GetXaxis().FindBin(0.))
+        h_fire.Divide(h_true)
+        return h_fire
 
     @staticmethod
     def _getEfficiencyValues(h_eff,delay):
@@ -131,9 +152,12 @@ class FileProcessor:
         eff         = h_eff.GetBinContent(bin_center,bin_delay) # Efficiency for this BX
         contNext    = h_eff.GetBinContent(bin_next,bin_delay)   # Contamination in next BX
         contPrev    = h_eff.GetBinContent(bin_prev,bin_delay)   # Contamination in previous BX
-
         return eff,contNext,contPrev
-    
+
+    @staticmethod
+    def _getFireRateValue(h_fire,delay):
+        return h_fire.GetBinContent(h_fire.GetXaxis().FindBin(delay))
+
     @staticmethod
     def _getHistContent(h):
         e = [h.GetXaxis().GetBinUpEdge(0)]
@@ -144,7 +168,7 @@ class FileProcessor:
             w.append(h.GetBinContent(i))
             s.append(h.GetBinError(i))
         return np.array(e),np.array(w),np.array(s)
- 
+
     @staticmethod
     def _fracOutHisto(e,w,s):
         c = (e[:-1]+e[1:])/2
@@ -155,11 +179,11 @@ class FileProcessor:
     def _meanHisto(e,w,s):
         c = (e[:-1]+e[1:])/2
         return (w*c).sum()/w.sum(),np.sqrt(((s*c)**2).sum())/w.sum()
- 
+
 
 
 class PlotScan:
-    def __init__(self,path,suffix,files,hists,parameters,efficiency,jobs,force,**kwargs):
+    def __init__(self,path,suffix,files,hists,parameters,observables,efficiency,firerate,jobs,force,**kwargs):
         self.files          = [os.path.join(path,f) for f in files]
         self.suffix         = suffix
         self.parameters     = parameters
@@ -167,18 +191,23 @@ class PlotScan:
         self.force          = force
         self.content        = []
         self.cache          = os.path.join(getEnv()['paths']['scans'],'cache',f'cache_{self.suffix}.pkl')
-        self.observables = ['Efficiency',
-                            'Out-of-time fraction',
-                            'Mean',
-                            'Next BX contamination',
-                            'Previous BX contamination']
-        self.limits = {'Efficiency'                 : (0.,1.),
-                       'Out-of-time fraction'       : (0.,1.),
-                       'Mean'                       : (-1.,1.),
-                       'Next BX contamination'      : (0.,1.),
-                       'Previous BX contamination'  : (0.,1.)}
 
-        self.processor = FileProcessor(hists,efficiency,parameters)
+
+        if observables is None:
+            self.observables = OBSERVABLES
+            self.ranges      = RANGES
+        else:
+            self.observables,self.ranges = [],{}
+            for obs in observables:
+                if obs not in OBSERVABLES:
+                    error_line =  f'Observable name `{obs}` is not defined, available observables are :\n'
+                    for OBSERVABLE in OBSERVABLES:
+                        error_line += f'... {OBSERVABLE}\n'
+                    raise RuntimeError(error_line)
+                self.observables.append(obs)
+                self.ranges[obs] = RANGES[obs]
+
+        self.processor = FileProcessor(hists,efficiency,firerate,parameters)
 
         self._getFullContent()
 
@@ -209,7 +238,7 @@ class PlotScan:
                     self.content.extend(content)
                 pool.close()
                 pool.join()
-                    
+
             df = pd.DataFrame(self.content)
             df.to_pickle(self.cache)
             logging.info(f'Saved cache in {self.cache}')
@@ -224,21 +253,30 @@ class PlotScan:
 
     def Plots(self):
         path_plots = os.path.join(getEnv()['paths']['scans'],'plots',self.suffix)
+        path_data  = os.path.join(getEnv()['paths']['scans'],'data',self.suffix)
         if not os.path.exists(path_plots):
             os.makedirs(path_plots)
+        if not os.path.exists(path_data):
+            os.makedirs(path_data)
         for obsName in self.observables:
             observable = self.data.GetObservable(obsName)
             for param in self.parameters.values():
                 logging.info(f"Plotting {obsName} curves of {param} parameter")
-                pdfDir = os.path.join(path_plots,f'curves_{obsName}_{param}'.replace(' ',''))
-                if not os.path.exists(pdfDir):
-                    os.makedirs(pdfDir)
+                output_plots_dir = os.path.join(path_plots,f'curves_{obsName}_{param}'.replace(' ',''))
+                output_data_dir = os.path.join(path_data,f'curves_{obsName}_{param}'.replace(' ',''))
+                if not os.path.exists(output_plots_dir):
+                    os.makedirs(output_plots_dir)
+                if not os.path.exists(output_data_dir):
+                    os.makedirs(output_data_dir)
 
                 labels_to_vary = {key:observable.GetLabels()[key] for key in observable.GetLabels().keys() if key != param and key != 'delay'}
 
                 for comb in itertools.product(*list(labels_to_vary.values())):
                     varied_labels = {k:c for k,c in zip(labels_to_vary.keys(),comb)}
                     obs2d = observable.GetSlice(**varied_labels)
+
+                    data_dict = {'2D':obs2d.GetRootTGraph2D(x='delay',y=param)}
+                    fig_name = f'{"_".join([f"{p}_{v}" for p,v in varied_labels.items()])}'.replace(' ','_')
 
                     # Plot 1D #
                     fig, ax = plt.subplots(figsize=(8,7))
@@ -248,25 +286,25 @@ class PlotScan:
                     for paramVal,color in zip(paramValues,colors):
                         obs1d = obs2d.GetSlice(**{param:paramVal})
                         obs1d.Pyplot1D(ax,color=color)
+                        data_dict[f'1D_{param}_{paramVal}'] = obs1d.GetRootTGraph()
                     sm = cm.ScalarMappable(cmap=cm.rainbow, norm=plt.Normalize(vmin=paramValues.min(), vmax=paramValues.max()))
                     cbar = plt.colorbar(sm)
                     cbar.set_label(param,fontsize=18,labelpad=20)
                     cbar.ax.tick_params(labelsize=14)
                     plt.xlabel('Delay [ns]',fontsize=18,labelpad=20)
                     plt.ylabel(obsName,fontsize=18,labelpad=10)
-                    plt.ylim(self.limits[obsName])
+                    plt.ylim(self.ranges[obsName])
                     title = f'{param} curves\n({", ".join([f"{p} = {v}" for p,v in varied_labels.items()])})'
                     plt.title(title,fontsize=20,pad=25)
                     plt.tick_params(axis='both', which='major', labelsize=14)
                     logging.info('... '+title.replace("\n"," : "))
-                    pdfName = os.path.join(pdfDir,f'{"_".join([f"{p}_{v}" for p,v in varied_labels.items()])}').replace(' ','_')
-                    fig.savefig(f'{pdfName}_1D.png')
+                    fig.savefig(f'{os.path.join(output_plots_dir,fig_name)}_1D.png')
                     plt.close()
 
                     # Plot 2D #
                     fig,ax = plt.subplots(figsize=(8,7))
-                    plt.subplots_adjust(left=0.15, right=0.95, top=0.85, bottom=0.1)
-                    obs2d.Pyplot2D(x='delay',y=param, ax=ax, vmin=self.limits[obsName][0], vmax=self.limits[obsName][1], shading='auto', linewidth=0,rasterized=True)
+                    plt.subplots_adjust(left=0.17, right=0.95, top=0.85, bottom=0.1)
+                    obs2d.Pyplot2D(x='delay',y=param, ax=ax, vmin=self.ranges[obsName][0], vmax=self.ranges[obsName][1], shading='auto', linewidth=0,rasterized=True)
                     cbar = fig.colorbar(ax.collections[0])
                     cbar.set_label(obsName,fontsize=18,labelpad=20)
                     cbar.ax.tick_params(labelsize=14)
@@ -274,10 +312,13 @@ class PlotScan:
                     ax.set_ylabel(param,fontsize=18,labelpad=20)
                     ax.set_title(title,fontsize=20,pad=25)
                     ax.tick_params(axis='both', which='major', labelsize=14)
-                    pdfName = os.path.join(pdfDir,f'{"_".join([f"{p}_{v}" for p,v in varied_labels.items()])}_2D.pdf')
-                    fig.savefig(f'{pdfName}_2D.png')
+                    fig.savefig(f'{os.path.join(output_plots_dir,fig_name)}_2D.png')
                     plt.close()
 
+                    # Save in ROOT file #
+                    with TFileOpen(f'{os.path.join(output_data_dir,fig_name)}.root','w') as F:
+                        for name,obj in data_dict.items():
+                            obj.Write(name)
 
 def main():
     parser = argparse.ArgumentParser(description='Produce datacards')
@@ -285,6 +326,8 @@ def main():
                         help='Yaml containing parameters')
     parser.add_argument('--custom', action='store', required=False, default=None, nargs='*',
                         help='Format the yaml file')
+    parser.add_argument('-o','--observables', action='store', required=False, default=None, nargs='*',
+                        help='List of observables to restrict the plotting to [default = all]')
     parser.add_argument('-v','--verbose', action='store_true', required=False, default=False,
                         help='Verbose mode')
     parser.add_argument('--cache', action='store_true', required=False, default=False,
@@ -305,7 +348,7 @@ def main():
         raise RuntimeError("Must provide the YAML file")
     f = parseYaml(args.yaml,args.custom)
 
-    instance = PlotScan(**f,jobs=args.jobs,force=args.cache)
+    instance = PlotScan(**f,jobs=args.jobs,force=args.cache,observables=args.observables)
     if not args.cache:
         instance.Plots()
 
